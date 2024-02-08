4da567ab00b4fcc62fe2844929cdd376c3fe6a62
Mutala Mutala <jonamut12.live@gmail.com>
2024-02-07 20:27:45 -0500

    entitie pour la domain layer

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product : BaseEntity 
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
