f1b6164fb841988c131c59628edfc078067cd86e
Mutala Mutala <jonamut12.live@gmail.com>
2024-02-09 01:13:56 -0500

    Ajout de la Business Logic pour le product dans l'application layer

--- Application/BusinessLogic/ProductBL.cs
+++ Application/BusinessLogic/ProductBL.cs
@@ -1,24 +1,26 @@
-﻿using Domain.Entities;
+﻿using Domain.Contracts;
+using Domain.Entities;
 using Domain.Interfaces;
+using Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Services
+namespace Application.BusinessLogic
 {
-    public class ProductService
+    public class ProductBL : IProductService
     {
         private readonly IProductRepository _productRepository;
 
-        public ProductService(IProductRepository productRepository)
+        public ProductBL(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
 
         /// <summary>
-        /// Methode permettant d'ajouter un produit 
+        /// Methode permettant d'ajouter un produit
         /// </summary>
         /// <param name="product"></param>
         public void AddProduct(Product product)
---
+++ Application/Dtos/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public  class ProductDto
+    {
+        public string? ProductName { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
