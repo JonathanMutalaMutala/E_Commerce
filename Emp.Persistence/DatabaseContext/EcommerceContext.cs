4106a5de0ead42c8c5f20d97bf5fc638d73f80e2
Mutala Mutala <jonamut12.live@gmail.com>
2024-02-10 23:48:39 -0500

    Ajout de l'infranstructure Layer et de la db context

---
+++ Emp.Persistence/DatabaseContext/EcommerceContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp.Persistence.DatabaseContext
+{
+    public  class EcommerceContext : DbContext
+    {
+        private readonly DbContextOptions<EcommerceContext> _options;
+
+
+        public EcommerceContext(DbContextOptions<EcommerceContext> options)
+        {
+            _options = options;
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Emp.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Domain.InterfacesRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp.Persistence.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
