9799c91744a17bf4601e275e709dc46713b03582
Mutala Mutala <jonamut12.live@gmail.com>
2024-02-11 10:00:03 -0500

    Change Extensions method  for the application layer

--- Application/Extensions/ApplicationExtensions.cs
+++ Application/Extensions/ApplicationExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.BusinessLogic;
+using Application.Contracts.Persistence;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +15,19 @@ namespace Application.Extensions
     {
         public static IServiceCollection AddApplicationServices(this  IServiceCollection services)
         {
+            var assembly = typeof(ApplicationExtensions).Assembly;
+
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
             //Ajout du service autoMapper à la collection des services 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+            // Ajout des Service pour le DInjection
+            services.AddScoped<IProductService, ProductBL>();
 
             return services;
         }
