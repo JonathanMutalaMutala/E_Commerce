94f9b8c263c2cf53b076459c341f26e3fb928b02
Mutala Mutala <jonamut12.live@gmail.com>
2024-02-11 09:51:59 -0500

    Add Product controller and Dependency Injection

---
+++ Emp.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Application.Contracts.Persistence;
+using Domain.InterfacesRepository;
+using Emp.Persistence.DatabaseContext;
+using Emp.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<EcommerceContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("EcommerceConnectionString"));
+            });
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services; 
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,11 @@
+using Application.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
